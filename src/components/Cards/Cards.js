ade2fdcee6cdc9144675b2cf182df9336fb31283
Duncan Moyo <duncanfmoyo@gmail.com>
2021-03-10 13:44:37 +0200

    home cards added

--- src/components/Cards/Cards.js
+++ src/components/Cards/Cards.js
@@ -1,48 +1,48 @@
 import CardItems from "./CardItems";
 import "./Cards.css";
-import ImageNine from "../../assets/images/img-9.jpg";
-import ImageThree from "../../assets/images/img-3.jpg";
-import ImageEight from "../../assets/images/img-8.jpg";
-import ImageTwo from "../../assets/images/img-2.jpg";
-import ImageFour from "../../assets/images/img-4.jpg";
+import ImageOne from "../../assets/images/ImageOne.jpg";
+import ImageTwo from "../../assets/images/ImageTwo.jpg";
+import ImageThree from "../../assets/images/ImageThree.jpg";
+import ImageFive from "../../assets/images/ImageFive.jpg";
+import ImageFour from "../../assets/images/ImageFour.jpg";
 
 const Cards = () => {
   return (
     <div className="cards">
-      <h1>Check out these EPIC destinations!</h1>
+      <h1>Causes we are serving</h1>
       <div className="cards__container">
         <div className="cards__wrapper">
           <ul className="cards__items">
             <CardItems
-              src={ImageNine}
-              text="Explore the hidden waterfall deep inside the Amazone Jungle"
-              label="Adventure"
+              src={ImageOne}
+              text="What led us to start this organisation"
+              label="News"
               path="/services"
             />
             <CardItems
               src={ImageTwo}
-              text="Travel through the Islands of Bali in a private Cruise"
-              label="Luxury"
+              text="Record Donations of Food Stuffs"
+              label="Food"
               path="/services"
             />
           </ul>
           <ul className="cards__items">
             <CardItems
               src={ImageThree}
-              text="Set Sail in the Atlantic Ocean visiting Uncharted Waters"
-              label="Mystery"
+              text="Breaking Boundaries and linking cultures"
+              label="Outreach"
               path="/services"
             />
             <CardItems
               src={ImageFour}
-              text="Experience Football on Top of the Himilayan Mountains"
-              label="Adventure"
+              text="Despite the virus, we still moved forward"
+              label="Distribution"
               path="/products"
             />
             <CardItems
-              src={ImageEight}
-              text="Ride through the Sahara Desert on a guided camel tour"
-              label="Adrenaline"
+              src={ImageFive}
+              text="More still needs to be done for the children"
+              label="Childcare"
               path="/sign-up"
             />
           </ul>
