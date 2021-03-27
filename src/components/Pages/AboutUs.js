24b7dd5130e5abfaf1a7e9ecd4cd808666bab554
Duncan Moyo <duncanfmoyo@gmail.com>
2021-03-27 23:47:51 +0200

    about us media queries fixed

--- src/components/Pages/AboutUs.js
+++ src/components/Pages/AboutUs.js
@@ -1,6 +1,5 @@
 import "../Cards/Cards.css";
-import {aboutUsData, womenDirectors} from "../data/aboutUsData";
-
+import { aboutUsData, womenDirectors } from "../data/aboutUsData";
 import { CardView } from "react-card-with-image";
 import "react-card-with-image/dist/index.css";
 
@@ -18,13 +17,13 @@ const AboutUs = () => {
       <h1 className="text">About Alakhe</h1>
       <div className="body">
         <div className="container">
-          <div style={{borderRadius: '10px'}} className="card">
+          <div style={{ borderRadius: "10px" }} className="card">
             <div className="imgBox">
               <img src={aboutUsIntroPicture} alt="women" />
             </div>
             <div className="content">
               <h1>{aboutUsTitle}</h1>
-              <p style={{marginTop: '15px'}} >{aboutUsContent}</p>
+              <p style={{ marginTop: "15px" }}>{aboutUsContent}</p>
               <br />
               <p>{aboutUsContentContinued}</p>
             </div>
@@ -36,32 +35,35 @@ const AboutUs = () => {
 
       {womenDirectors.map(({ id, header, description, image }) => {
         return (
-          <div key={id} className="body">
-            <div className="container">
-              <div style={{borderRadius: '10px'}} className="card">
-                <div className="imgBox">
-                  <img src={image} alt="careerWomen" />
-                </div>
-                <div className="content">
-                  <h1>{header}</h1>
-                  <p style={{marginTop: '15px'}} >{description}</p>
+          <div key={id} className="mobile">
+            <div className="body">
+              <div className="container">
+                <div style={{ borderRadius: "10px" }} className="card">
+                  <div className="imgBox">
+                    <img src={image} alt="careerWomen" />
+                  </div>
+                  <div className="content">
+                    <h1>{header}</h1>
+                    <p style={{ marginTop: "15px" }}>{description}</p>
+                  </div>
                 </div>
               </div>
             </div>
           </div>
         );
       })}
-
-      <div className="cards__container">
-        <div className="cards__wrapper">
-          <div className="cards__items">
-            <CardView
-              items={womenDirectors}
-              activeColor="#000"
-              imageHeight="100%"
-              imageWidth="100%"
-            />
-          </div>
+      <div className="computer">
+        <div className="cards__container">
+          
+            <div className="cards__items">
+              <CardView
+                items={womenDirectors}
+                activeColor="#000"
+                imageHeight="100%"
+                imageWidth="100%"
+              />
+            </div>
+          
         </div>
       </div>
     </>
--- src/components/data/aboutUsData.js
+++ src/components/data/aboutUsData.js
@@ -1,49 +1,70 @@
-import womanOne from '../../assets/images/womanOne.jpg'
-import womanTwo from '../../assets/images/womanTwo.jpg'
-import womanThree from '../../assets/images/womanThree.jpg'
-import womanFour from '../../assets/images/womanFour.jpg'
-import womanFive from '../../assets/images/womanFive.jpg'
-import women from '../../assets/images/women.jpg'
+import womanOne from "../../assets/images/womanOne.jpg";
+import womanTwo from "../../assets/images/womanTwo.jpg";
+import womanThree from "../../assets/images/womanThree.jpg";
+import womanFour from "../../assets/images/womanFour.jpg";
+import womanFive from "../../assets/images/womanFive.jpg";
+import women from "../../assets/images/women.jpg";
+import womanSix from "../../assets/images/womanSix.jpg";
+import womanSeven from "../../assets/images/womanSeven.jpg";
 
 const aboutUsData = {
-  aboutUsIntroPicture : women,
+  aboutUsIntroPicture: women,
   aboutUsTitle: "Who We Are...",
-  aboutUsContent: "Alakhe is an organization recently registered as an NPO after a group of young professionals saw it fit to expand their portfolio and formalise their process to other students after successfully mentoring young women through their respective degree portfolios at the University of Cape Town and at two high schools in Johannesburg.",
-  aboutUsContentContinued: "The organization is an NPO run by 7 women based in Cape Town, Johannesburg and Port Elizabeth each with different expertise. We largely aim to support young women through their career and educational journeys both through educational mentorship and social assistance.",
-}
+  aboutUsContent:
+    "Alakhe is an organization recently registered as an NPO after a group of young professionals saw it fit to expand their portfolio and formalise their process to other students after successfully mentoring young women through their respective degree portfolios at the University of Cape Town and at two high schools in Johannesburg.",
+  aboutUsContentContinued:
+    "The organization is an NPO run by 7 women based in Cape Town, Johannesburg and Port Elizabeth each with different expertise. We largely aim to support young women through their career and educational journeys both through educational mentorship and social assistance.",
+};
 
 const womenDirectors = [
   {
+    id: 5,
+    header: "Samukelisiwe Nyamathe",
+    description:
+      "Samke Nyamathe is a General Medical Practitioner by profession. Her  passion is seeing women and children realise their full health potential regardless of their socio-economic status. She believes that health care should be accessible and provided in a professional manner to all who access it. As a young doctor, Samke recognizes the importance of innovation in medicine. She obtained her medical qualification from the University of Cape Town and went on to do her Diploma in Public Health at UNISA and her Masters in Child Health at Wits University. She completed internship and community service at Far East Rand Hospital and South Rand Hospital respectively. She then worked as a private general practitioner doing physical and virtual consultations. She has worked for an NGO called Hope Cape Town which focused on paediatric HIV and neurodevelopment. She currently works within clinical research at the Desmond Tutu TB Centre and has assisted and worked in different areas of clinical research.",
+    image: `${womanFive}`,
+  },
+  {
     id: 1,
     header: "Theresa Mwesigwa",
-    description: "Terry is someone who is passionate about seeing all people, especially young people, become the best version of themselves and realise their fullest potential. An SSP scholar from the class of 2010, she studied Medicine at the University of Cape Town as a Gallagher Foundation scholar . She went on to complete her internship at Leratong Hospital and community service at the National Institute for Communicable Diseases. Her passion for youth development led her to co-found WAME Projects, an NPO that runs career education and sexual & reproductive health workshops for high school students in under-resourced areas, while her love for public health has seen her serve as the National Chairperson of the Junior Doctors Association of South Africa. Her real calling in life, however, is playing the acoustic guitar… or at least pretending to. Terry currently works as a management consultant at Bain & Company South Africa.",
+    description:
+      "Terry is someone who is passionate about seeing all people, especially young people, become the best version of themselves and realise their fullest potential. An SSP scholar from the class of 2010, she studied Medicine at the University of Cape Town as a Gallagher Foundation scholar . She went on to complete her internship at Leratong Hospital and community service at the National Institute for Communicable Diseases. Her passion for youth development led her to co-found WAME Projects, an NPO that runs career education and sexual & reproductive health workshops for high school students in under-resourced areas, while her love for public health has seen her serve as the National Chairperson of the Junior Doctors Association of South Africa. Her real calling in life, however, is playing the acoustic guitar… or at least pretending to. Terry currently works as a management consultant at Bain & Company South Africa.",
     image: `${womanOne}`,
   },
   {
     id: 2,
     header: "Khanyisile Nyamathe",
-    description: "Born and raised in Johannesburg, South Africa, Khanyisile Nyamathe is a Grade 4 English, Social Science and Technology teacher. She obtained her BEd at Tuks (UP), and is in the process of completing her BEd Honors at the same institution. Khanyi is also the founder of an educational consulting initiative - ‘Classroom in a Caravan’. Through this, she equips high school students with resources which they use to plan for life after high school. These resources include subject selection workshops, advice about how to choose a study route, and study skills, to name a few. The aim of this initiative is to equip the under-resourced with information in an attempt to see more young South Africans in-the-know about their plans after high school, avoiding drastic growth of the youth unemployment and crime rates. Khanyi believes that through education and mentorship, we can make a meaningful difference and children’s lives can be forever changed.",
+    description:
+      "Born and raised in Johannesburg, South Africa, Khanyisile Nyamathe is a Grade 4 English, Social Science and Technology teacher. She obtained her BEd at Tuks (UP), and is in the process of completing her BEd Honors at the same institution. Khanyi is also the founder of an educational consulting initiative - ‘Classroom in a Caravan’. Through this, she equips high school students with resources which they use to plan for life after high school. These resources include subject selection workshops, advice about how to choose a study route, and study skills, to name a few. The aim of this initiative is to equip the under-resourced with information in an attempt to see more young South Africans in-the-know about their plans after high school, avoiding drastic growth of the youth unemployment and crime rates. Khanyi believes that through education and mentorship, we can make a meaningful difference and children’s lives can be forever changed.",
     image: `${womanTwo}`,
   },
   {
     id: 3,
     header: "Clementine Van Der Walt",
     description: "Clementine Van Der Walt is an educator/ tutor born in Johannesburg, South Africa. Clemmy holds a BA in Psychology Degree and a BA Honours Degree. She obtained her qualifications from the University of Johannesburg. She began her teaching career in the North of Johannesburg while teaching at Jacaranda college. She gained three years of teaching experience at Jacaranda College. She then began here journey with tutoring high school students and spent one year tutoring in Tembisa for an organization called Pursuit and one year tutoring matriculants at a non-governmental organization called the Johannesburg Society for the Blind. Currently Clementine freelances as a private tutor and still tutors for the Johannesburg Society for the Blind.",
-    descriptionContinued: "Clementine Van Der Walt is an educator/ tutor born in Johannesburg, South Africa. Clemmy holds a BA in Psychology Degree and a BA Honours Degree. She obtained her qualifications from the University of Johannesburg. She began her teaching career in the North of Johannesburg while teaching at Jacaranda college. She gained three years of teaching experience at Jacaranda College. She then began here journey with tutoring high school students and spent one year tutoring in Tembisa for an organization called Pursuit and one year tutoring matriculants at a non-governmental organization called the Johannesburg Society for the Blind. Currently Clementine freelances as a private tutor and still tutors for the Johannesburg Society for the Blind.",
     image: `${womanThree}`,
   },
   {
     id: 4,
     header: "Segopotso Maleho ",
-    description: "Segopotso Maleho is an educator who was born and raised in Taung in the North West Province of South Africa. She is a mother of two amazing children who are her motivation and driving force, Oratile Khotso and Oarabile Thapelo. She obtained her Bachelor of Education (FET) qualification from the University of Johannesburg. She started her teaching journey at Glen Kids where she gained three years of teaching experience. She then moved to Rambrandt Park Primary School where she spent one year. She has been a permanent teacher at Glenvista High School for the past four years. Sego currently teaches grade 8 and 9 Economic Management Sciences and grade 10, 11 and 12 Tourism at Glenvista High School.",
+    description:
+      "Segopotso Maleho is an educator who was born and raised in Taung in the North West Province of South Africa. She is a mother of two amazing children who are her motivation and driving force, Oratile Khotso and Oarabile Thapelo. She obtained her Bachelor of Education (FET) qualification from the University of Johannesburg. She started her teaching journey at Glen Kids where she gained three years of teaching experience. She then moved to Rambrandt Park Primary School where she spent one year. She has been a permanent teacher at Glenvista High School for the past four years. Sego currently teaches grade 8 and 9 Economic Management Sciences and grade 10, 11 and 12 Tourism at Glenvista High School.",
     image: `${womanFour}`,
   },
   {
-    id: 5,
-    header: "Samukelisiwe Nyamathe",
-    description: "Samke Nyamathe is a General Medical Practitioner by profession. Her  passion is seeing women and children realise their full health potential regardless of their socio-economic status. She believes that health care should be accessible and provided in a professional manner to all who access it. As a young doctor, Samke recognizes the importance of innovation in medicine. She obtained her medical qualification from the University of Cape Town and went on to do her Diploma in Public Health at UNISA and her Masters in Child Health at Wits University. She completed internship and community service at Far East Rand Hospital and South Rand Hospital respectively. She then worked as a private general practitioner doing physical and virtual consultations. She has worked for an NGO called Hope Cape Town which focused on paediatric HIV and neurodevelopment. She currently works within clinical research at the Desmond Tutu TB Centre and has assisted and worked in different areas of clinical research.",
-    image: `${womanFive}`,
+    id: 6,
+    header: "Sinovuyo Msutu",
+    description:
+      "Sinovuyo Msutu is a 29-year-old queer black woman from a small town in the Eastern Cape called Tabankulu. In 2009 she moved to Cape Town to study medicine at UCT, a decision that was not entirely driven by her passions or desires but one that, nonetheless, shaped her tremendously. She currently works as a psychiatry registrar at Weskoppies hospital in Pretoria, Gauteng. Her passion in life is black women. She is passionate about their happiness, their physical and mental well-being, their stories, their safety and everything that will lead to their success. She wants to see black women afforded the choices that they also deserve to have. She wants to see them given the respect that they deserve by society. She wants to see black women living healthy and wholesome lives. “I will dedicate my career and my entire life to making sure that black women win.” – Sinovuyo Msutu.",
+    image: `${womanSix}`,
+  },
+  {
+    id: 7,
+    header: " Lisa Dondashe ",
+    description:
+      "Dr Lisa Dondashe was born and bred in Uitenhage, Eastern Cape and attended Riebeek College Girls’ High where she matriculated as the head girl in 2009. She went on to complete her medical degree at the University of Cape Town in 2015. In both high school and university, she always ensured to immerse herself in the cultural and academic extra-mural activities. After completing internship in Gauteng and her community service in Port Elizabeth, Eastern Cape, her love for psychiatry emerged. She’s currently working as a medical officer and will soon transfer to Cape Town to pursue her aspirations of specializing in psychiatry as a registrar at the University of Cape Town. Lisa is also a Masters’ candidate in Bioethics and Health Law and she believes this has contributed towards her becoming a better clinician and advocate for her patients. ",
+    image: `${womanSeven}`,
   },
 ];
 
-export {womenDirectors, aboutUsData}
+export { womenDirectors, aboutUsData };
